a70ec0f2e219efc3a66db60e929aed799d3dd4c2
a1678991 <a1678991@gmail.com>
2016-12-10 10:23:48 +0900

    add feature

--- cloudatcost-cli.sh
+++ cloudatcost-cli.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 # !!!! set MAIL and KEY !!!!
-
-MAIL="none"
-KEY="none"
+MAIL=""
+KEY=""
 
 if [ $MAIL = "none" ] || [ $KEY = "none" ]; then
-	echo "Please set MAIL and KEY!!"
-	exit
+        echo "Please set MAIL and KEY!!"
+        exit
 fi
 if which jq >/dev/null 2>&1; then
 
+show_usage ()
+{
+    echo "Usage:"
+    echo "  -m MailAdress"
+    echo "  -k api key"
+    echo "  -l list servers"
+    echo "  -r show resouces"
+    exit 1
+}
 get_resources () {
 	RESOURCES=`curl -k -s "https://panel.cloudatcost.com/api/v1/cloudpro/resources.php?key=$KEY&login=$MAIL" | jq .data`
 	TOTAL=`echo $RESOURCES | jq .total`
@@ -24,9 +32,43 @@ get_resources () {
 	AVAILABLE_RAM=`expr $TOTAL_RAM - $USED_RAM`
 	AVAILABLE_STORAGE=`expr $TOTAL_STORAGE - $USED_STORAGE`
 }
+list_servers () {
+curl -k -s -X GET "https://panel.cloudatcost.com/api/v1/listservers.php?key=$KEY&login=$MAIL" | jq '.data[] | {SID: .sid, name: .servername, Mode: .mode, IP: .ip, OS: .template, Status: .status, Pass: .rootpass, Host: .hostname}'
+}
+show_resources () {
+        get_resources
+        echo "TotalCPU: $TOTAL_CPU UsedCPU: $USED_CPU AvailableCPU: $AVAILABLE_CPU"
+        echo "TotalRAM: $TOTAL_RAM MB UsedRAM: $USED_RAM MB AvailableRAM: $AVAILABLE_RAM MB"
+        echo "TotalSSD: $TOTAL_STORAGE GB UsedSSD: $USED_STORAGE GB AvailableSSD: $AVAILABLE_STORAGE GB"
+}
+select_server() {
+                echo "Server List"
+                curl -k -s -X GET "https://panel.cloudatcost.com/api/v1/listservers.php?key=$KEY&login=$MAIL" | jq '.data[] | {SID: .sid, name: .servername, Mode: .mode, IP: .ip, OS: .template, Status: .status}'
+                echo -n "Enter server SID :"
+                read SID
+}
+
+#option
+while getopts :m:k:l:r:h opt
+do
+    case $opt in
+        m)      MAIL=$OPTARG
+                ;;
+        k)      KEY=$OPTARG
+                ;;
+        l)      list_servers && exit 0
+                ;;
+        r)      show_resources && exit 0
+                ;;
+        h)      show_usage
+                ;;
+        \?)     show_usage
+                ;;
+        esac
+done
 
 if [ -z "$OPETYPE" ]; then
-	echo -n "Choose oepration [B]uild/[L]istserver/[D]eleteserver/check[R]esources:/Run[M]ode :"
+	echo -n "Choose oepration [B]uild/[L]istserver/[D]eleteserver/check[R]esources:/Run[M]ode/Enter[C]onsole :"
 	read OPETYPE
 fi
 if [ $OPETYPE = "b" ] || [ $OPETYPE = "B" ]; then
@@ -54,17 +96,11 @@ if [ $OPETYPE = "b" ] || [ $OPETYPE = "B" ]; then
 	fi
 
 	curl -k -s -X POST https://panel.cloudatcost.com/api/v1/cloudpro/build.php --data "key=$KEY&login=$MAIL&cpu=$CPU&ram=$RAM&storage=$STORAGE&os=$OS" |jq .
-  
-  
+
 elif [ $OPETYPE = "l" ] || [ $OPETYPE = "L" ]; then
-	curl -k -s -X GET "https://panel.cloudatcost.com/api/v1/listservers.php?key=$KEY&login=$MAIL" | jq '.data[] | {SID: .sid, name: .servername, Mode: .mode, IP: .ip, OS: .template, Status: .status, Pass: .rootpass, Host: .hostname}'
+	list_servers
 elif [ $OPETYPE = "d" ] || [ $OPETYPE = "D" ]; then
-	if [ -z "$SID" ]; then	
-		echo "Server List"
-		curl -k -s -X GET "https://panel.cloudatcost.com/api/v1/listservers.php?key=$KEY&login=$MAIL" | jq '.data[] | {SID: .sid, name: .servername, Mode: .mode, IP: .ip, OS: .template, Status: .status}'
-		echo -n "Enter server SID :"
-		read SID
-	fi
+	select_server
 	if [ -z "$CONFIRM" ]; then
 		echo "Really delete this server ?"
 		echo "SID: $SID"
@@ -75,24 +111,20 @@ elif [ $OPETYPE = "d" ] || [ $OPETYPE = "D" ]; then
 		curl -k -s -X POST https://panel.cloudatcost.com/api/v1/cloudpro/delete.php --data "key=$KEY&login=$MAIL&sid=$SID" | jq
 	fi
 elif [ $OPETYPE = "r" ] || [ $OPETYPE = "R" ]; then
-	get_resources
-	echo "TotalCPU: $TOTAL_CPU UsedCPU: $USED_CPU AvailableCPU: $AVAILABLE_CPU"
-	echo "TotalRAM: $TOTAL_RAM MB UsedRAM: $USED_RAM MB AvailableRAM: $AVAILABLE_RAM MB"
-	echo "TotalSSD: $TOTAL_STORAGE GB UsedSSD: $USED_STORAGE GB AvailableSSD: $AVAILABLE_STORAGE GB"
+	show_resources
 elif [ $OPETYPE = "m" ] || [ $OPETYPE = "M" ]; then
-	if [ -z "$SID" ]; then
-		echo "Server List"
-		curl -k -s -X GET "https://panel.cloudatcost.com/api/v1/listservers.php?key=$KEY&login=$MAIL" | jq '.data[] | {SID: .sid, name: .servername, Mode: .mode, IP: .ip, OS: .template, Status: .status}'
-		echo -n "Enter server SID :"
-		read SID
-	fi
+	select_server
 	if [ -z "$MODE" ]; then
 		echo -n "Enter mode [normal/safe] :"
 		read MODE
 	fi
 	curl -k -s -X POST https://panel.cloudatcost.com/api/v1/runmode.php --data "key=$KEY&login=$MAIL&sid=$SID&mode=$MODE" | jq
+elif [ $OPETYPE = "c" ] || [ $OPETYPE = "C" ]; then
+	select_server
+	curl -k -X POST https://panel.cloudatcost.com/api/v1/console.php --data "key=$KEY&login=$MAIL&sid=$SID"
 fi
-  
+
+
 else
 	echo "Please install jq (https://stedolan.github.io/jq/)"
 fi
